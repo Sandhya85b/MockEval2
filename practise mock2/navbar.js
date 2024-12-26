71cfb02d7a50b9a980cdb98d16ddd80cf43220dc
Sandhya85b <sandhya85.b@gmail.com>
2024-12-26 20:10:09 +0530

    This is my pratice code on mock2

---
+++ practise mock2/navbar.js
@@ -0,0 +1,11 @@
+
+export function navbarlinks(){
+    const navbar=`
+    <nav>
+    <a href="index.html">Home</a>
+    <a href="quiz.html">Quiz</a>
+    <a href="questions.html></a>
+    </nav>
+    `;
+    document.getElementById('navbar').innerHTML=navbar
+}
